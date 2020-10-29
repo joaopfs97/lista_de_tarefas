b80d6551f5d57827699d9e7f2cddcb53f6a8094f
joaopfs97 <joaopfs201@gmail.com>
2020-10-29 00:02:16 -0300

    Finalizado aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,21 @@ class _HomeState extends State<Home> {
       _saveData();
     });
   }
+
+  Future<Null> _refresh() async{
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a,b){
+        if(a["ok"] && !b["ok"]) return 1;
+        else if(!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+      _saveData();
+    });
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,10 +91,10 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
+            child: RefreshIndicator(child: ListView.builder(
                 padding: EdgeInsets.only(top: 10),
                 itemCount: _toDoList.length,
-                itemBuilder: buildItem),
+                itemBuilder: buildItem), onRefresh: _refresh)
           )
         ],
       ),
@@ -130,6 +145,7 @@ class _HomeState extends State<Home> {
             },),
             duration: Duration(seconds: 2),
           );
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
