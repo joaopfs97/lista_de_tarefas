348866d67f61e3fefcdd250de469fc0337580826
joaopfs97 <joaopfs201@gmail.com>
2020-10-28 23:50:09 -0300

    Configurando lógica para exclusão das tarefas

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
 
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
   @override
   void initState() {
@@ -109,6 +111,28 @@ class _HomeState extends State<Home> {
             });
           },
         ),
+      onDismissed: (direction){
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["Title"]}\" removida!"),
+            action: SnackBarAction(label:"Desfazer",
+              onPressed: (){
+              setState(() {
+                _toDoList.insert(_lastRemovedPos,_lastRemoved);
+                _saveData();
+              });
+            },),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
